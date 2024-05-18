276d2a817cb320daf1ba131928498af8afc4cb3f
NAGARAJU SWARNA <36414037+nagswarnaa@users.noreply.github.com>
2024-05-18 10:41:07 -0500

    Time: 1995 ms (14.66%), Space: 82.8 MB (13.65%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -6,17 +6,14 @@ class Solution:
         return self.findProfit(prices,0, 1, noOfPrices,dp,noOfTrns)
 
     def findProfit(self,prices, idx, isBuy, noOfPrices,dp,noOfTrns):
-        if idx==noOfPrices:
+        if idx==noOfPrices or noOfTrns>2:
             return 0
-        if noOfTrns<=2:
-            if dp[idx][isBuy][noOfTrns]!=-1:
-                return dp[idx][isBuy][noOfTrns]
-            if isBuy:
-                profit=max(-prices[idx]+self.findProfit(prices, idx+1, 0, noOfPrices,dp,noOfTrns+1),self.findProfit(prices, idx+1, 1, noOfPrices,dp,noOfTrns))
-            else:
-                profit=max(prices[idx]+self.findProfit(prices, idx+1, 1, noOfPrices,dp,noOfTrns),self.findProfit(prices, idx+1, 0, noOfPrices,dp,noOfTrns))
+        if dp[idx][isBuy][noOfTrns]!=-1:
+            return dp[idx][isBuy][noOfTrns]
+        if isBuy:
+            profit=max(-prices[idx]+self.findProfit(prices, idx+1, 0, noOfPrices,dp,noOfTrns+1),self.findProfit(prices, idx+1, 1, noOfPrices,dp,noOfTrns))
         else:
-            return 0
+            profit=max(prices[idx]+self.findProfit(prices, idx+1, 1, noOfPrices,dp,noOfTrns),self.findProfit(prices, idx+1, 0, noOfPrices,dp,noOfTrns))
         dp[idx][isBuy][noOfTrns]=profit
         return dp[idx][isBuy][noOfTrns]
 
