60a8f34273473a6d79b1921cc14960dd3ba4c48c
NAGARAJU SWARNA <36414037+nagswarnaa@users.noreply.github.com>
2024-05-18 09:49:07 -0500

    Time: 70 ms (7.08%), Space: 25.2 MB (5.46%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,11 +1,23 @@
+from typing import List
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        x=prices[0]
-        max_profit=0
-        for i in range(1,len(prices)):
-            if prices[i]>x:
-                max_profit+=(prices[i]-x)
-                x=prices[i]
-            else:
-                x=min(x,prices[i])
-        return max_profit
+        n = len(prices)
+        dp = [[-1] * 2 for _ in range(n)]
+        return self.findMaxProfit(prices, 0, 1, dp)
+    
+    def findMaxProfit(self, prices, idx, isBuy, dp):
+        if idx == len(prices):
+            return 0
+        if dp[idx][isBuy] != -1:
+            return dp[idx][isBuy]
+        
+        if isBuy:
+            profit = max(-prices[idx] + self.findMaxProfit(prices, idx + 1, 0, dp), 
+                         self.findMaxProfit(prices, idx + 1, 1, dp))
+        else:
+            profit = max(prices[idx] + self.findMaxProfit(prices, idx + 1, 1, dp), 
+                         self.findMaxProfit(prices, idx + 1, 0, dp))
+        
+        dp[idx][isBuy] = profit
+        return dp[idx][isBuy]
